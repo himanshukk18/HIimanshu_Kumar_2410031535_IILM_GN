b95f22c1ea2bc97160b103ad2d2986a0fcad3046
himanshukk18 <himanshu.kumar3.cs28@iilm.edu>
2026-02-24 09:32:27 +0530

    Add files via upload

---
+++ EXP6.py
@@ -0,0 +1,13 @@
+def rotate_array(arr):
+    last = arr[-1]
+
+    for i in range(len(arr) - 1, 0, -1):
+        arr[i] = arr[i - 1]
+
+    arr[0] = last
+    return arr
+
+
+# Example
+arr = [1, 2, 3, 4, 5]
+print(rotate_array(arr))
