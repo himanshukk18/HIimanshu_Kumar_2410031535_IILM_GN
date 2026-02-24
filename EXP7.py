76f7f117c066ef5aacebf1424ad68e00e3620ec9
himanshukk18 <himanshu.kumar3.cs28@iilm.edu>
2026-02-24 09:33:09 +0530

    Add files via upload

---
+++ EXP7.py
@@ -0,0 +1,14 @@
+def max_subarray_sum(arr):
+    current_sum = arr[0]
+    max_sum = arr[0]
+
+    for i in range(1, len(arr)):
+        current_sum = max(arr[i], current_sum + arr[i])
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
+
+
+# Example
+arr = [2, 3, -8, 7, -1, 2, 3]
+print(max_subarray_sum(arr))
