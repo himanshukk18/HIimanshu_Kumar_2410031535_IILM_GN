8793c38cf27c3e581f432efe400b90f5d1d477cf
himanshukk18 <himanshu.kumar3.cs28@iilm.edu>
2026-02-24 09:54:48 +0530

    Add files via upload

---
+++ EXP16.py
@@ -0,0 +1,13 @@
+def rotate_by_one(arr):
+    n = len(arr)
+    last_element = arr[n - 1]
+
+    for i in range(n - 1, 0, -1):
+        arr[i] = arr[i - 1]
+
+    arr[0] = last_element
+    return arr
+
+
+arr6 = [1, 2, 3, 4, 5]
+print(rotate_by_one(arr6))
